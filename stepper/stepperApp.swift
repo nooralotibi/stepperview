b05effdfc6fd73a71c467e65a3f053ad6f36b7ff
nooreealotibi <nooreealotibi@gmail.com>
2023-03-18 00:16:17 +0300

    Initial Commit

---
+++ stepper/stepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepperApp.swift
+//  stepper
+//
+//  Created by noor alotibi on 26/08/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct stepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
